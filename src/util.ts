5c599ca1914c2346eb23904343d423b4c22cc95a
神代綺凛 <i@loli.best>
2023-08-13 15:59:42 +0800

    fix: string error

--- src/util.ts
+++ src/util.ts
@@ -12,3 +12,19 @@ export function int(num: number) {
 export function clip(data: number, min: number, max: number) {
   return data < min ? min : data > max ? max : data;
 }
+
+export function b64Encode(str: string) {
+  const te = new TextEncoder();
+  const buffer = te.encode(str);
+  return btoa(String.fromCharCode(...buffer));
+}
+
+export function b64Decode(data: string) {
+  const bin = atob(data);
+  const arr: number[] = [];
+  for (let i = 0; i < bin.length; i++) {
+    arr.push(bin.charCodeAt(i));
+  }
+  const td = new TextDecoder();
+  return td.decode(Uint8Array.from(arr));
+}
